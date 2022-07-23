0a358d2d672ddb55432b560dd181157fee82e001
Anton Zubov QAMID-42 <bakaphoton@gmail.com>
2022-07-23 16:23:27 +0300

    zadanie 1

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        BonusMilesService calculate = new BonusMilesService();
+        int miles = calculate.xz(10000);
+        System.out.println("Миль: " + miles);
+
+    }
+}
